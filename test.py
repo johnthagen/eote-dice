88602787e5bff12acfeb842bfbf0f2d45a7191be
johnthagen <johnthagen@gmail.com>
2016-02-15 14:01:00 -0500

    Add some tests for Dice and DicePool.

--- test.py
+++ test.py
@@ -4,7 +4,9 @@
 
 import unittest
 
-from dice import dice_from_color_char, ProficiencyDice, Side, Symbol
+# from eote_dice import parse_arguments
+from dice import (AbilityDice, BoostDice, ChallengeDice, DicePool, dice_from_color_char,
+                  DifficultyDice, ProficiencyDice, SetbackDice, Side, Symbol)
 
 
 class SideTestCase(unittest.TestCase):
@@ -22,7 +24,46 @@ class DiceFromColorTestCase(unittest.TestCase):
 
     def test_valid_dice_char(self):
         self.assertIsInstance(dice_from_color_char('y'), ProficiencyDice)
+        self.assertIsInstance(dice_from_color_char('b'), BoostDice)
+        self.assertIsInstance(dice_from_color_char('g'), AbilityDice)
+        self.assertIsInstance(dice_from_color_char('k'), SetbackDice)
+        self.assertIsInstance(dice_from_color_char('p'), DifficultyDice)
+        self.assertIsInstance(dice_from_color_char('r'), ChallengeDice)
 
+
+class DiceTestCase(unittest.TestCase):
+    def test_num_sides(self):
+        self.assertEqual(ProficiencyDice().num_sides(), 12)
+        self.assertEqual(BoostDice().num_sides(), 6)
+
+    def test_boost_roll(self):
+        symbols = BoostDice().roll()
+        self.assertGreaterEqual(len(symbols), 0)
+        self.assertLessEqual(len(symbols), 2)
+        if len(symbols) > 0:
+            self.assertIsNot(symbols[0], Symbol.Triumph)
+            self.assertIsNot(symbols[0], Symbol.Despair)
+            self.assertIsNot(symbols[0], Symbol.Failure)
+            self.assertIsNot(symbols[0], Symbol.Threat)
+
+    def test_difficulty_roll(self):
+        symbols = DifficultyDice().roll()
+        self.assertGreaterEqual(len(symbols), 0)
+        self.assertLessEqual(len(symbols), 2)
+        if len(symbols) > 0:
+            self.assertIsNot(symbols[0], Symbol.Triumph)
+            self.assertIsNot(symbols[0], Symbol.Despair)
+            self.assertIsNot(symbols[0], Symbol.Success)
+            self.assertIsNot(symbols[0], Symbol.Advantage)
+
+
+class DicePoolTestCase(unittest.TestCase):
+    def test_from_string(self):
+        DicePool.from_string('yygbrppk')
+
+    def test_invalid_from_string(self):
+        with self.assertRaises(ValueError):
+            DicePool.from_string('yyx')
 
 if __name__ == '__main__':
     unittest.main()
