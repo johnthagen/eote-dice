2bff2e4477009934984bec5880b71937a0d44055
johnthagen <johnthagen@gmail.com>
2018-07-09 20:40:56 -0400

    Support Python 3.6 and 3.7 and PyPy

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Games/Entertainment :: Role-Playing',
     ],
 
